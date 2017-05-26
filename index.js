811627ae9b19ec12e853bcdee8a853aaf5f50d18
n0iz <n0iz@n0iz.ddns.net>
2017-05-26 19:42:32 +0200

    add npm scipts

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-var Web3 = require('web3')
+const contracts_location = "../contracts"
 
-if (typeof web3 !== 'undefined') {
-	web3 = new Web3(web3.currentProvider)
-} else {
-	// set the provider you want from Web3.providers
-	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
-	web3.eth.getBlock(48, function(error, result) {
-	    if(!error)
-	        console.log(result)
-	    else
-	        console.error(error);
-	})
-}
+var Web3 = require('web3')
+var Produce = require(contracts_location+"/build/contracts/ProductionRegistry.json")
+var contract = require("truffle-contract")
+var web3Provider = new Web3.providers.HttpProvider("http://localhost:8545")
+
+var MyContract = contract(Produce)
+MyContract.setProvider(web3Provider)
+
+MyContract.deployed().then(function(instance) {
+	console.log(instance)
+}).catch(function (e) {
+	console.log(e)
+})
+
+MyContract.deployed().then(function(instance) {
+	return instance.getProductionsCount({from: "0x1337"}) // <-- matches the doStuff() function within MyContract.sol.
+}).then(function(result) {
+	// We just made a transaction, and it's been mined!
+	// We're given transaction hash, logs (events) and receipt for further processing.
+	console.log(result.tx, result.logs, result.receipt);
+}).catch(function (e) {
+	console.log(e)
+})
