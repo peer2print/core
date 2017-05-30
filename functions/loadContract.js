587d29a1f6750556e0ca6f3fd573af2f477c07aa
n0izn0iz <n0izn0iz@users.noreply.github.com>
2017-05-30 15:23:56 +0200

    Update loadContract.js

--- functions/loadContract.js
+++ functions/loadContract.js
@@ -3,13 +3,10 @@ var contract = require("truffle-contract")
 var Web3 = require('web3')
 var web3Provider = new Web3.providers.HttpProvider("http://localhost:8545")
 
-
 const contracts = require("contracts");
 
 module.exports = function (name)
 {
-	console.log(contracts)
-	console.log(contracts[name+".json"])
 	var ct = contract(contracts[name+".json"])
 	ct.setProvider(web3Provider)
 	return ct
